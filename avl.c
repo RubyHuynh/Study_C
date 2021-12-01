9f096b3ad91f9703e27f2fc1ed5fdf160e500a0e
Ruby Huynh <hthn.oo@gmail.com>
2021-12-01 14:01:55 +0000

    AVL tree delete recursive

--- avl.c
+++ avl.c
@@ -20,7 +20,6 @@ int height(Node* node) {
 Node* newNode(int key) {
 	Node* item = (Node*) calloc(1, sizeof(Node));
 	item->key = key;
-	item->left = item->right = NULL;
 	item->height = 1;
 	return item;
 }
@@ -34,6 +33,7 @@ Node* rightRotate(Node* node) {
 
 	node->height = max(height(node->left), height(node->right)) + 1;
 	left->height = max(height(left->left), height(left->right)) + 1;
+	printf("\t%s old root (%d) new root(%d) \n", __func__, node->key, left->key);
 
 	return left;
 }
@@ -48,7 +48,7 @@ Node* leftRotate(Node* node) {
 
 	node->height = max(height(node->left), height(node->right)) + 1;
 	right->height = max(height(right->left), height(right->right)) + 1;
-
+	printf("\t%s old root (%d) new root(%d) \n", __func__, node->key, right->key);
 	return right;
 }
 
@@ -59,9 +59,11 @@ int getBalance(Node* node) {
 
 Node* insert(Node* node, int key) {
 	int balance;
-	
+
 	if (!node) return newNode(key);
-	
+
+	printf("%s current root (%d), adding (%d)\n", __func__, node->key, key);
+
 	if (key < node->key) 
 		node->left = insert(node->left, key);
 	else if (key > node->key)
@@ -92,6 +94,70 @@ Node* insert(Node* node, int key) {
 	return node;
 }
 
+struct Node * minVal(struct Node* node)
+{
+	struct Node* current = node;
+
+	/* loop down to find the leftmost leaf */
+	while (current->left != NULL)
+		current = current->left;
+
+	return current;
+}
+
+Node* delete(Node* root, int key) {
+	int balance;
+	
+	if (!root) return root;
+
+	if (key < root->key)
+		root->left = delete(root->left, key);
+	else if (key > root->key)
+		root->right = delete(root->right, key);
+	else {
+		if (!root->left || !root->right) {
+			Node* tmp = root->left ? root->left : root->right;
+			if (!tmp) {
+				tmp = root;
+				root = NULL;
+			}
+			else {
+				*root = *tmp;
+			}
+
+			free(tmp);
+		}
+		else {
+			Node* tmp = minVal(root->right);
+			root->key = tmp->key;
+			root->right = delete(root->right, tmp->key);
+		}
+	}
+	if (!root) {
+		return root;
+	}
+	root->height = 1 + max(height(root->left), height(root->right));
+	balance = getBalance(root);
+	if (balance > 1 && getBalance(root->left) >= 0)
+		return rightRotate(root);
+	if (balance > 1 && getBalance(root->left) < 0)
+	{
+		root->left =  leftRotate(root->left);
+		return rightRotate(root);
+	}
+	if (balance < -1 && getBalance(root->right) <= 0)
+		return leftRotate(root);
+
+	if (balance < -1 && getBalance(root->right) > 0)
+	{
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
+	}
+
+	return root;
+}
+
+
 void dump(Node* root) {
 	if (root) {
 		printf("%d\t", root->key);
@@ -111,6 +177,11 @@ int main() {
 	root = insert(root, 25);
 
 	printf("dump AVL:\n");
+	dump(root);
+	printf("\n");
+
+	root = delete(root, 40);
+	printf("dump AVL after delete:\n");
 	dump(root);
 	printf("\n");
 	return 0;
